a2cb86b0d9d07bec7f5b0c50984844073f41c92a
Puneet Sharma <88921054+Puneet26072001@users.noreply.github.com>
2025-01-29 23:59:02 +0530

    Add files via upload

--- SOLID Principles/Open Closed Principle/Code without Open Closed Principle/InvoiceDao.java
+++ SOLID Principles/Open Closed Principle/Code without Open Closed Principle/InvoiceDao.java
@@ -1,1 +1,10 @@
-
+class InvoiceDao{
+  UpdatedInvoice invoice;
+  public InvoiceDao(UpdatedInvoice UpdatedInvoice)
+  {
+    this.invoice=  UpdatedInvoice;
+  }
+  public void SaveToDB(){
+    // code to save to DB
+  }
+}
