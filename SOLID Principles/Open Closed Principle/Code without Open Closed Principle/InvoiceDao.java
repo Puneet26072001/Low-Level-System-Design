e07a50db151ca1957d9e290fdcdb1dc0b2fc28aa
Puneet Sharma <88921054+Puneet26072001@users.noreply.github.com>
2025-01-30 00:00:34 +0530

    Update InvoiceDao.java

--- SOLID Principles/Open Closed Principle/Code without Open Closed Principle/InvoiceDao.java
+++ SOLID Principles/Open Closed Principle/Code without Open Closed Principle/InvoiceDao.java
@@ -7,4 +7,7 @@ class InvoiceDao{
   public void SaveToDB(){
     // code to save to DB
   }
+  public void SaveToFile(){
+    // code to save to File // Mofication is Done which voilates Open closed Principle
+  }
 }
