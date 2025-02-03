1e3be86da6ef0e8a523bd2006a07596b8574e50b
Puneet Sharma <88921054+Puneet26072001@users.noreply.github.com>
2025-02-03 22:02:07 +0530

    Add files via upload

---
+++ SOLID Principles/Liskov Substitution Principle/Code with Liskov Substitution Principle/EngineVehicle.java
@@ -0,0 +1,14 @@
+public class EngineVehicle extends Vehicle {
+
+  // Follow the steps below to use this file
+
+  // 1. In the main file, create an instance of this class:
+  // NewClass1 instance1 = new NewClass1();
+
+  // 2. Call the method to get the greeting message:
+  // System.out.println(instance1.sayHelloFromNewClass());
+  public Boolean engineStarts(){
+    return true;
+  }
+
+}
---
+++ SOLID Principles/Liskov Substitution Principle/Code with Liskov Substitution Principle/Vehicle.java
@@ -0,0 +1,15 @@
+public class Vehicle {
+
+  // Follow the steps below to use this file
+
+  // 1. In the main file, create an instance of this class:
+  // NewClass1 instance1 = new NewClass1();
+
+  // 2. Call the method to get the greeting message:
+  // System.out.println(instance1.sayHelloFromNewClass());
+
+  public Integer getNumberOfWheels(){
+    return 2;
+  }
+
+}
