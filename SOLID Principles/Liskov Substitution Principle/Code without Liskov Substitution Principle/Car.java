502336b42bad7ddc75f6d5873058b9ee1384eadf
Puneet Sharma <88921054+Puneet26072001@users.noreply.github.com>
2025-02-03 21:49:53 +0530

    Add files via upload

---
+++ SOLID Principles/Liskov Substitution Principle/Code without Liskov Substitution Principle/Car.java
@@ -0,0 +1,15 @@
+public class Car extends Vehicle {
+
+  // Follow the steps below to use this file
+
+  // 1. In the main file, create an instance of this class:
+  // NewClass1 instance1 = new NewClass1();
+
+  // 2. Call the method to get the greeting message:
+  // System.out.println(instance1.sayHelloFromNewClass());
+  @Override
+  public Integer getNumberOfWheels(){
+    return 4;
+  }
+
+}
